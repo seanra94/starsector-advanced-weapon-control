98368a6f1b4dc8a2a476d299125c836296db0de1
seanra94 <seanra94@users.noreply.github.com>
2026-05-01 11:52:12 +1000

    Fix PrioBig to remain priority-only without ship-only validity

--- src/main/kotlin/com/dp/advancedgunnerycontrol/weaponais/tags/PrioritizeBigTag.kt
+++ src/main/kotlin/com/dp/advancedgunnerycontrol/weaponais/tags/PrioritizeBigTag.kt
@@ -2,7 +2,6 @@ package com.dp.advancedgunnerycontrol.weaponais.tags
 
 import com.dp.advancedgunnerycontrol.weaponais.FiringSolution
 import com.dp.advancedgunnerycontrol.weaponais.bigness
-import com.fs.starfarer.api.combat.CombatEntityAPI
 import com.fs.starfarer.api.combat.ShipAPI
 import com.fs.starfarer.api.combat.WeaponAPI
 
@@ -11,8 +10,6 @@ class PrioritizeBigTag(weapon: WeaponAPI) : WeaponAITagBase(weapon) {
         val targetShip = solution.target as? ShipAPI ?: return 1f
         return 1f / bigness(targetShip)
     }
-
-    override fun isBaseAiValid(entity: CombatEntityAPI): Boolean = entity is ShipAPI
 
     override fun shouldFire(solution: FiringSolution): Boolean = true
 
